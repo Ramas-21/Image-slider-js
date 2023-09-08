759ce6b57d696037770565bb5fe1ff1fff3299c1
Lawrence <lawrencehome21@gmail.com>
2023-09-08 12:14:35 -0700

    recorded the initial cursor and scroll position of the carousel

--- script.js
+++ script.js
@@ -1,15 +1,24 @@
 const carousel = document.querySelector(".carousel");
 
-let isDragging = false;
+let isDragging = false, startX, startScrollLeft;
 
 const dragStart = () => {
     isDragging = true;
-    carousel.classList.add("dragging")
+    carousel.classList.add("dragging");
+    // Records the initial cursor and scroll position of the carousel
+    startX = e.pageX;
+    startScrollLeft = carousel.scrollLeft;
 }
 
 const dragging = (e) => {
     if(!isDragging) return; // if isDragging is false return from here
     carousel.scrollLeft = e.pageX;
 }
+
+const dragStop = () => {
+    isDragging = false;
+    carousel.classList.remove("dragging");
+}
 carousel.addEventListener("mousedown", dragStart);
 carousel.addEventListener("mousemove", dragging);
+document.addEventListener("mouseup", dragStop);
