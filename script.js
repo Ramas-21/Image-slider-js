3c763c6b8e19983ffcb5eb0bf729d74cb7c20ebe
Lawrence <lawrencehome21@gmail.com>
2023-09-08 11:51:15 -0700

    An if condition to be returned IsDragging is false

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 const carousel = document.querySelector(".carousel");
+
+let isDragging = false;
+
 const dragging = (e) => {
+    if(!isDragging) return; // if isDragging is false return from here
     carousel.scrollLeft = e.pageX;
 }
 carousel.addEventListener("mousemove", dragging);
